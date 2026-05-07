d13ce93f5a3045af6a183faf7a4be65b77e0e3d9
luqmannnn <luqmannurhakimt@gmail.com>
2026-05-07 19:55:59 +0800

    fix 2 checkov issues

--- main.tf
+++ main.tf
@@ -23,3 +23,53 @@ terraform {
 resource "aws_s3_bucket" "s3_tf" {
   bucket_prefix = "luqman-ce12-7may-bucket"
 }
+
+resource "aws_s3_bucket_lifecycle_configuration" "s3_tf_lifecycle" {
+  bucket = aws_s3_bucket.s3_tf.id
+
+  rule {
+    id     = "expire-objects"
+    status = "Enabled"
+
+    expiration {
+      days = 30
+    }
+  }
+}
+
+resource "aws_s3_bucket" "logging" {
+  bucket = "luqman-ce12-32-logging-bucket"
+}
+
+data "aws_iam_policy_document" "logging_bucket_policy" {
+  statement {
+    principals {
+      identifiers = ["logging.s3.amazonaws.com"]
+      type        = "Service"
+    }
+    actions   = ["s3:PutObject"]
+    resources = ["${aws_s3_bucket.logging.arn}/*"]
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceAccount"
+      values   = [data.aws_caller_identity.current.account_id]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "logging" {
+  bucket = aws_s3_bucket.logging.bucket
+  policy = data.aws_iam_policy_document.logging_bucket_policy.json
+}
+
+resource "aws_s3_bucket_logging" "example" {
+  bucket = aws_s3_bucket.s3_tf.bucket
+
+  target_bucket = aws_s3_bucket.logging.bucket
+  target_prefix = "log/"
+  target_object_key_format {
+    partitioned_prefix {
+      partition_date_source = "EventTime"
+    }
+  }
+}

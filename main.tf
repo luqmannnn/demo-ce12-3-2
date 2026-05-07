7c8db3615ed506281545b03cf01da58f1b9213ee
luqmannnn <luqmannurhakimt@gmail.com>
2026-05-07 19:44:16 +0800

    update errors for lint

--- main.tf
+++ main.tf
@@ -3,12 +3,22 @@ provider "aws" {
 }
 
 terraform {
+  required_version = "~> 1.15.0"
+  
   backend "s3" {
     bucket = "sctp-ce12-tfstate-bucket" # Change this
     key    = "luqman-ce12-7may.tfstate" # Change this
     region = "ap-southeast-1"
   }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.44.0"
+    }
+  }
 }
+
 
 resource "aws_s3_bucket" "s3_tf" {
   bucket_prefix = "luqman-ce12-7may-bucket"
